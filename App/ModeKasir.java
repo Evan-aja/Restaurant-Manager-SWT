1307bcb771dd6a340141d5e5bdee15a63eed3f9a
Evan-aja <gebel909@student.ub.ac.id>
2021-05-10 11:36:40 +0700

    dispose window button fix

--- App/ModeKasir.java
+++ App/ModeKasir.java
@@ -123,6 +123,7 @@ public class ModeKasir extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
